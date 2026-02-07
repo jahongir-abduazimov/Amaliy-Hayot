1fd628cc7124b3cf4b0c421f0c8ae652927ca533
jahongirabduazimov707 <jahongirabduazimov707@gmail.com>
2026-02-07 13:08:04 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import CookieBanner from "@/components/CookieBanner";
+// import CookieBanner from "@/components/CookieBanner";
 import ConditionalScripts from "@/components/ConditionalScripts";
 
 const geistSans = Geist({
